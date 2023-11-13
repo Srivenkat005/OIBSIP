f70f4fec0d7f4f75903e9066563291b2774fecc0
Srivenkat005 <150649691+Srivenkat005@users.noreply.github.com>
2023-11-13 16:45:53 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,12 @@
+function logout() {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+  
+    window.location.href = "login.html";
+  }
+  var usernameDisplay = document.getElementById("usernameDisplay");
+  if (localStorage.getItem("username")) {
+    usernameDisplay.textContent = localStorage.getItem("username");
+  } else {
+    window.location.href = "login.html";
+}
